0fd27bc92b3207d2f4b3f9b6102522e9ea9430ae
Jinwoo Han <qew8502@gmail.com>
2025-02-10 16:15:25 +0900

    remove unused config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from "next";
-// import path from "path";
-const nextConfig: NextConfig = {
-  // sassOptions: {
-  //   includePaths: [path.join(__dirname, "styles")],
-  // },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
